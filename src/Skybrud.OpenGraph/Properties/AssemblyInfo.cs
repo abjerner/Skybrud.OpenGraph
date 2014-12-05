56ddecf4dc9c0fe57213687b79f2213f458b01bb
Anders Bjerner <abjerner@skybrud.dk>
2014-12-05 11:21:13 +0100

    Updated the Assembly information

--- src/Skybrud.OpenGraph/Properties/AssemblyInfo.cs
+++ src/Skybrud.OpenGraph/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Skybrud.OpenGraph")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Small library for generating Open Graph meta tags")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Skybrud.dk")]
 [assembly: AssemblyProduct("Skybrud.OpenGraph")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
